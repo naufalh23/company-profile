5df9fe9b7ad71f04673c19b56102d67bec438ffb
Naufal Hesantra <n.hesantra12@gmail.com>
2024-08-05 22:27:07 +0700

    update footer

---
+++ src/app/components/copyright.tsx
@@ -0,0 +1,11 @@
+export default function CopyrightBot() {
+  return (
+    <footer className="footer footer-center bg-base-300 text-base-content p-4">
+      <aside>
+        <p>
+          Copyright © {new Date().getFullYear()} - All right reserved by Naufal H
+        </p>
+      </aside>
+    </footer>
+  );
+}
